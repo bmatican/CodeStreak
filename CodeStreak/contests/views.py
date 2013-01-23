f818f2cff34829f2af71e32e8ebf1cd339fca2eb
Szymon Sidor <szymon.sidor@gmail.com>
2013-01-23 19:25:20 +0100

    Pulling data now works. Restricted data requires you to be admin. Sample html in static/tmp

--- CodeStreak/contests/views.py
+++ CodeStreak/contests/views.py
@@ -2,6 +2,8 @@ import json
 
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.http import Http404
+
 
 from CodeStreak.xhpy import *
 
@@ -19,14 +21,42 @@ def test(request, test_id=0):
 
   return HttpResponse(str(page))
 
+def getScores():
+  return {
+            'scores' : 'scores',
+         }
+
+def getLogs():
+  return {
+            'logs' : 'logs',
+         }
+
+data_providers = {
+  'getScores' : getScores,
+}
+
+restricted_data_providers = {
+  'getLogs' : getLogs, 
+}
+
 def pula(request):
+  global data_providers
   if request.is_ajax() and request.method == 'POST':
-    test = request.POST.get('test', '')
-    response = {
-      'test' : test
-    }
-    return HttpResponse(json.dumps(response))
+    action = request.POST.get('action', '')
+    response = None
+    if action in data_providers:
+      data_function = data_providers.get(action)
+      response = data_function()
+    if (response == None and action in restricted_data_providers and 
+        request.user.is_staff):
+      data_function = restricted_data_providers.get(action)
+      response = data_function()
+    if response == None:
+        raise Http404
+    else:
+      return HttpResponse(json.dumps(response))
     #return render_to_response('pula/response.html', locals())
   else:
-    pass # should render 404
+    raise Http404
+
     
